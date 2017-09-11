65101aaeb35e9ea642ab876205d447235613f007
YuShuaiGuang <1019418872@qq.com>
2017-09-11 11:20:13 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/test/yushuaigaung1506d20170911/bean/CartBean.java
@@ -0,0 +1,16 @@
+package com.bwie.test.yushuaigaung1506d20170911.bean;
+/**
+ * 用途：CartBean类
+ * 姓名：于帅光
+ * 时间：2017/9/11
+ */
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+
+
+}
